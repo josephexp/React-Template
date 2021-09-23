1268cfeb219303f66179889e449ae5478d7a52de
josephexp <joseph.antony@experionglobal.com>
2021-09-23 18:58:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import Store from './store';
+import AppRoutes from './router';
+
+function App() {
+	return (
+		<Provider store={Store}>
+			<AppRoutes />
+		</Provider>
+	);
+}
+
+export default App;
